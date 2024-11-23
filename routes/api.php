dc5cea05d781223fec2314ac5d6e6f2757a57b4f
Pindraa <kadekpindra@gmail.com>
2024-11-24 00:07:15 +0800

    Delete register Route for API

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('schedules', ScheduleController::class);
 });
 
-Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
