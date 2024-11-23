a797e0b0f527f3b69b4407767c7236b6069820e9
Pindraa <kadekpindra@gmail.com>
2024-11-23 23:49:29 +0800

    Create Models for Student

---
+++ app/Models/Student.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'classroom_id', 'absen', 'nis'];
+
+    public function classroom()
+    {
+        return $this->belongsTo(Classroom::class);
+    }
+}
