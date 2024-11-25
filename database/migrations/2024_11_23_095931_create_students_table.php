620df9da4d3882d9d8d5ed373003bc0367890552
Pindraa <kadekpindra@gmail.com>
2024-11-25 14:14:28 +0800

    Change Students Table

--- database/migrations/2024_11_23_095931_create_students_table.php
+++ database/migrations/2024_11_23_095931_create_students_table.php
@@ -4,22 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStudentsTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    public function up(): void
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Hubungan dengan tabel users
             $table->string('name');
-            $table->foreignId('classroom_id')->constrained()->onDelete('cascade'); 
-            $table->integer('absen');
-            $table->string('nis')->unique();
+            $table->string('nis')->unique(); // NIS siswa
+            $table->foreignId('classroom_id')->constrained()->onDelete('cascade'); // Hubungan dengan kelas
             $table->timestamps();
         });
     }
 
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('students');
     }
-}
+};
