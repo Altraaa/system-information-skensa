ff94d29c7290db1b122e33f199625bc61539089e
Pindraa <kadekpindra@gmail.com>
2024-11-25 22:20:38 +0800

    Adding row Room in Schedules Table

--- database/migrations/2024_11_23_071720_create_schedules_table.php
+++ database/migrations/2024_11_23_071720_create_schedules_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('classroom_id')->constrained()->onDelete('cascade');
             $table->foreignId('subject_id')->constrained()->onDelete('cascade');
             $table->foreignId('teacher_id')->constrained()->onDelete('cascade');
+            $table->string('room');
             $table->string('day');
             $table->time('start_time');
             $table->time('end_time');
