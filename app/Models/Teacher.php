031c67c4a2c0e1781d3cd84d7575098ad6db5517
Pindraa <kadekpindra@gmail.com>
2024-11-25 22:22:53 +0800

    Revise row and Add Relation with User Table

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -4,19 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class Teacher extends Model
 {
     use HasFactory;
 
     protected $fillable = [
+        'nip',
         'name',
-        'email',
+        'user_id',
         'subject_id',
     ];
 
     public function subject()
     {
         return $this->belongsTo(Subject::class);
+    }
+
+    // Relasi ke tabel User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
